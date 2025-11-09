9d3526b2a1a8fd3cddba388765b20be46a61ba9e
ollpej <OllCyl@users.noreply.github.com>
2025-11-09 21:04:49 +0100

    lang and env

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'ollcyl.github.io/Perceptual-Systems/',
+  base: process.env.VITE_BASE_PATH || '/perceptual-systems/',
 })
